23488532167dca9112652dbd2e55165b69189e6a
Abimayyu <86156899+Abimayyu@users.noreply.github.com>
2025-03-03 00:20:55 +0700

    fix: user seeder and readme.md

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -15,23 +15,27 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('12345678'),
             'remember_token' => Str::random(10),
             'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
 
         // Create Pemesan User
-        User::create([
+        DB::table('users')->insert([
             'name' => 'Pemesan User',
             'email' => 'pemesan@example.com',
             'email_verified_at' => now(),
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
             'role' => 'pemesan',
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
